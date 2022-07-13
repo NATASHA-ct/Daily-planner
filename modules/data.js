dd29bbf90f93142da45c672984acb2de5a863c19
NATASHA-ct <chirombenatashatatenda@gmail.com>
2022-07-13 04:58:29 +0300

    Added js modules

---
+++ modules/data.js
@@ -0,0 +1,16 @@
+// Array of Objects with projects area properties
+
+const listItems = [
+  {
+    description: 'wash the dishes',
+    completed: 'checked',
+    index: 0,
+  },
+
+  {
+    description: 'complete To Do list project',
+    completed: 'unchecked',
+    index: 1,
+  },
+];
+export default listItems;
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    index: "./src/index.js",
+    index: './src/index.js',
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  mode: "development",
+  mode: 'development',
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
 
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
